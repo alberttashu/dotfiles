5c3618e4bb59b36e3fc437329cd26058ba419b2f
a.tashu <tashualbert@gmail.com>
2025-05-09 12:07:37 +0200

    some nvim updates

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 DIR=$(dirname $(realpath $0))
 
 ln -s $DIR/nvim ~/.config/nvim
+ln -s $DIR/aerospace ~/.config/aerospace
 ln -s $DIR/code/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s $DIR/code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s $DIR/.ideavimrc ~/.ideavimrc
